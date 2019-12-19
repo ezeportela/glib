40d5534528f588d87d2e4b62db681d5adaa3ab90
Ezequiel Portela <eportelab@gmail.com>
2019-12-19 16:12:40 -0300

    Fix

--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,5 @@
-const entityValidationHandler = require('../src/middlewares/entityValidationHandler');
-const validateTokenProps = require('../src/middlewares/validateTokenProps');
+const entityValidationHandler = require('./src/middlewares/entityValidationHandler');
+const validateTokenProps = require('./src/middlewares/validateTokenProps');
 const validateTokenValid = require('./src/middlewares/validateTokenValid');
 
 module.exports = {
