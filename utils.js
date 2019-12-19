6465c1b7406f8f91ddd53f841277ce50aa73f8f4
Ezequiel Portela <eportelab@gmail.com>
2019-12-19 12:22:30 -0300

    Refactor

--- utils.js
+++ utils.js
@@ -1,11 +1,11 @@
 const conversions = require('./src/utils/conversions');
 const errorHandler = require('./src/utils/errorHandler');
 const requireDependency = require('./src/utils/requireDependency');
-const timespent = require('./src/utils/timespent');
+const Timespent = require('./src/utils/timespent');
 
 module.exports = {
   conversions,
   errorHandler,
   requireDependency,
-  timespent,
+  Timespent,
 };
