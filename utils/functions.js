0c36a13cbab347759811958d899efe54e889a914
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-01-08 11:06:50 -0300

    Fix util functions

--- utils/functions.js
+++ utils/functions.js
@@ -1,14 +1,9 @@
 const originIsInCORS = (req, res, cors) => {
-  console.log('pre-function', req.headers, cors);
   if (cors.length == 0) return;
 
   const url = cors.filter((url) => url === req.headers.origin);
-  console.log('function url', url);
 
-  if (url) {
-    console.log('set response header', url);
-    res.set('Access-Control-Allow-Origin', url);
-  }
+  if (url) res.set('Access-Control-Allow-Origin', url);
 };
 
 module.exports = {
