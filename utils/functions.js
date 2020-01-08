23ab49537072f847cd56dbd4f7e6ef7bdbbfaf99
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-01-08 11:02:05 -0300

    Temp change

--- utils/functions.js
+++ utils/functions.js
@@ -1,9 +1,14 @@
 const originIsInCORS = (req, res, cors) => {
+  console.log('pre-function', req.headers, cors);
   if (cors.length == 0) return;
 
   const url = cors.filter((url) => url === req.headers.origin);
+  console.log('function url', url);
 
-  if (url) res.set('Access-Control-Allow-Origin', cors);
+  if (url) {
+    console.log('set response header', url);
+    res.set('Access-Control-Allow-Origin', url);
+  }
 };
 
 module.exports = {
