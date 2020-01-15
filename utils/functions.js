f1f0b5868a2ddbd6e3a612cde6054ed04bb16ed5
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-01-15 12:59:29 -0300

    Add createResponse function

--- utils/functions.js
+++ utils/functions.js
@@ -1,3 +1,5 @@
+const {getError} = require('./errorHandler');
+
 const originIsInCORS = (req, res, cors) => {
   if (cors.length == 0) return;
 
@@ -6,6 +8,14 @@ const originIsInCORS = (req, res, cors) => {
   if (url) res.set('Access-Control-Allow-Origin', url);
 };
 
+const createResponse = ({success, err, data, isProd, details}) => ({
+  success,
+  error: err ? getError(err) : null,
+  data,
+  details: isProd ? details : null,
+});
+
 module.exports = {
   originIsInCORS,
+  createResponse,
 };
--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,12 @@
 const conversions = require('./conversions');
 const errorHandler = require('./errorHandler');
 const requireDependency = require('./requireDependency');
-const {originIsInCORS} = require('./functions');
+const {originIsInCORS, createResponse} = require('./functions');
 
 module.exports = {
   conversions,
   errorHandler,
   requireDependency,
   originIsInCORS,
+  createResponse,
 };
